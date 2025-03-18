d9f5e4a368e73895de1e69de422217f80b6a177e
Deansie <dennis.andersen@lerniastudent.se>
2025-03-18 09:48:49 +0100

    Added two text strings with conversion to all uppercase-lettering

--- algorithms.js
+++ algorithms.js
@@ -1,6 +1,16 @@
 export function algorithmA() {
+    
+    const guessedWord = "noisy";
+    const correctWord = "hoist"
+
+    const upperGuess = guessedWord.toUpperCase();
+    const upperCorrect = correctWord.toUpperCase();
+
+    console.log(upperGuess, upperCorrect);
 
 };
+
+algorithmA();
 
 export function algorithmB() {
 
