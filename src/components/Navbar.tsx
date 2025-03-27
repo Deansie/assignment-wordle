42e65a2dd5ab115688dfab4cce2aba66377ea4cf
Deansie <dennis.andersen@lerniastudent.se>
2025-03-27 10:03:47 +0100

    Add eventlistener for hover on links in navigataion using State

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,29 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
+    const [hoveredLink, setHoveredLink] = useState<string | null>(null);
     return (
         <nav>
             <ul>
-                <li>
+                <li
+                    onMouseEnter={() => setHoveredLink('/')}
+                    onMouseLeave={() => setHoveredLink(null)}
+                >
                     <Link to="/">Home</Link>
                 </li>
 
-                <li>
+                <li
+                    onMouseEnter={() => setHoveredLink("/highscores")}
+                    onMouseLeave={() => setHoveredLink(null)}
+                >
                     <Link to="/highscores">Highscores</Link>
                 </li>
 
-                <li>
+                <li
+                    onMouseEnter={() => setHoveredLink("/about")}
+                    onMouseLeave={() => setHoveredLink(null)}
+                >
                     <Link to="/about">About</Link>
                 </li>
             </ul>
