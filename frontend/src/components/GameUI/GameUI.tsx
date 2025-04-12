0a8edab0bad05725bb7fe8d95ef324d243cead20
Deansie <dennis.andersen@lerniastudent.se>
2025-04-12 21:10:51 +0200

    Added mocked wordlist in GameUI.tsx and filtering of words based on selected difficulty

--- frontend/src/components/GameUI/GameUI.tsx
+++ frontend/src/components/GameUI/GameUI.tsx
@@ -2,6 +2,13 @@ import { useState } from "react";
 import { ReactNode } from 'react';
 import './GameUI.css';
 
+// Mocked wordlist initially
+const wordLists: { [key: number]: string[] } = {
+    4: ['CAKE', 'BITE', 'FUSE', 'GLOW'],
+    5: ['APPLE', 'HOUSE', 'CANDY', 'BREAD', 'FLAME'],
+    6: ['CANDLE', 'BRIDGE', 'FROZEN'],
+  };
+
 interface GameUIProps {
     letterCount: number;
     allowRepeatingLetters: boolean;
@@ -12,6 +19,11 @@ export default function GameUI({ letterCount, allowRepeatingLetters }: GameUIPro
     return (
     <div className='gameUiDiv'>
         <h2>Game started</h2>
+        <table className="gameBoard">
+            <tbody>
+
+            </tbody>
+        </table>
         <span>
             <p>{letterCount} {allowRepeatingLetters ? 'yes' : 'no'} </p>
         </span>
