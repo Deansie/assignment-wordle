8538454907ebfc4118fdd01433be4aed18b91a6b
Deansie <dennis-andersen@outlook.com>
2025-07-16 20:45:17 +0200

    fix(index.js): correction to cors and mongodb uri again

--- backend/index.js
+++ backend/index.js
@@ -8,12 +8,12 @@ import escapeHtml from 'escape-html';
 
 const app = express();
 const port = process.env.PORT || 5081;
-const uri = 'mongodb://192.168.0.82:27017';
+const uri = 'mongodb://192.168.0.82:27017/wordleGame';
 const client = new MongoClient(uri);
 
 const gameSessions = new Map();
 
-app.use(cors({ origin: 'https://deansie-wordle.ekedala-services.se' }));
+app.use(cors({ origin: ['https://deansie-wordle.ekedala-services.se', 'http://deansie-wordle.ekedala-services.se', 'http://localhost:8080'] }));
 app.use(express.json());
 
 app.use(express.static("../frontend/dist"));
